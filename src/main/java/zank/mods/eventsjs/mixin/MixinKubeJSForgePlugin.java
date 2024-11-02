59fc9944bccd90a969b750d628fbe11e035c8801
ZZZank <3410764033@qq.com>
2024-11-02 11:13:44 +0800

    reload and redirect original `onPlatformEvent`

---
+++ src/main/java/zank/mods/eventsjs/mixin/MixinKubeJSForgePlugin.java
@@ -0,0 +1,38 @@
+package zank.mods.eventsjs.mixin;
+
+import dev.latvian.kubejs.forge.BuiltinKubeJSForgePlugin;
+import dev.latvian.kubejs.forge.KubeJSForgeEventHandlerWrapper;
+import dev.latvian.kubejs.script.BindingsEvent;
+import lombok.val;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.EventPriority;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import zank.mods.eventsjs.ClassConvertible;
+import zank.mods.eventsjs.SidedNativeEvents;
+
+/**
+ * @author ZZZank
+ */
+@Mixin(value = BuiltinKubeJSForgePlugin.class, remap = false)
+public abstract class MixinKubeJSForgePlugin {
+
+    @Inject(method = "onPlatformEvent", at = @At("HEAD"), cancellable = true)
+    private static void replace(BindingsEvent event, Object[] args, CallbackInfoReturnable<Object> cir) {
+        if (args.length < 2 || !(args[0] instanceof CharSequence)) {
+            throw new RuntimeException("Invalid syntax! onPlatformEvent(string, function) required event class and handler");
+        }
+
+        try {
+            val type = (Class) Class.forName(args[0].toString());
+            val handler = (KubeJSForgeEventHandlerWrapper) args[1];
+            SidedNativeEvents.STARTUP.onEvent(ClassConvertible.fromRaw(type), handler);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+
+        cir.setReturnValue(null);
+    }
+}
---
+++ src/main/java/zank/mods/eventsjs/mixin/MixinScriptManager.java
@@ -0,0 +1,27 @@
+package zank.mods.eventsjs.mixin;
+
+import dev.latvian.kubejs.script.ScriptManager;
+import dev.latvian.kubejs.script.ScriptType;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import zank.mods.eventsjs.SidedNativeEvents;
+
+/**
+ * @author ZZZank
+ */
+@Mixin(value = ScriptManager.class, remap = false)
+public class MixinScriptManager {
+
+    @Shadow
+    @Final
+    public ScriptType type;
+
+    @Inject(method = "unload", at = @At("TAIL"))
+    public void onUnload(CallbackInfo ci) {
+        SidedNativeEvents.byType(this.type).unload();
+    }
+}
