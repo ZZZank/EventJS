97d57167a858c0001a642bb403923b4e305744df
ZZZank <3410764033@qq.com>
2024-11-02 13:38:26 +0800

    rename `NativeEvent` binding to `NativeEvents`

--- src/main/java/zank/mods/eventjs/EventJSKubeJSPlugin.java
+++ src/main/java/zank/mods/eventjs/EventJSKubeJSPlugin.java
@@ -18,6 +18,6 @@ public class EventJSKubeJSPlugin extends KubeJSPlugin {
 
     @Override
     public void addBindings(BindingsEvent event) {
-        event.add("NativeEvent", SidedNativeEvents.byType(event.type));
+        event.add("NativeEvents", SidedNativeEvents.byType(event.type));
     }
 }
