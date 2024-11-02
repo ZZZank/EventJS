04bf4f3fa7908fd4bc7988071288e53e58f5e044
ZZZank <3410764033@qq.com>
2024-11-02 17:28:26 +0800

    mod event support

--- src/main/java/zank/mods/eventjs/EventJSMod.java
+++ src/main/java/zank/mods/eventjs/EventJSMod.java
@@ -1,6 +1,11 @@
 package zank.mods.eventjs;
 
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.IModBusEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 /**
  * @author ZZZank
@@ -10,4 +15,16 @@ public class EventJSMod {
 
     public static final String MOD_ID = "eventjs";
     public static final String NAME = "EventJS";
+
+    static IEventBus MOD_BUS;
+
+    public EventJSMod(FMLJavaModLoadingContext context) {
+        MOD_BUS = context.getModEventBus();
+    }
+
+    public static IEventBus selectBus(Class<? extends Event> eventType) {
+        return IModBusEvent.class.isAssignableFrom(eventType)
+            ? MOD_BUS
+            : MinecraftForge.EVENT_BUS;
+    }
 }
